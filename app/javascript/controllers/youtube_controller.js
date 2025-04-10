971e63fdc6738fb7f124a825eb889145fe0f13ae
dueityou <dueityou@gmail.com>
2025-04-10 15:18:26 +0900

    Display Error Message when there is no input in the URL form

--- app/javascript/controllers/youtube_controller.js
+++ app/javascript/controllers/youtube_controller.js
@@ -1,7 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "url" ]
+  static targets = [ 
+                    "url",
+                    "error_url",
+   ]
+
+  url_validation() {
+    console.log(this.urlTarget.value)
+    const urlInput = this.urlTarget
+    const urlError = this.error_urlTarget
+
+    if(this.urlTarget.value === "") {
+      console.log("中身がありませんよ")
+      urlError.className = "text-red-500 border border-red-400"
+      urlError.textContent = "put youtube video URL here"
+    } else {
+      urlError.className = ""
+      urlError.textContent = ""
+    }
+  }
 
   fetch_url() {
     console.log(this.urlTarget.value)
