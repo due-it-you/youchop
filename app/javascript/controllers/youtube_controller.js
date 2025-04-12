f3d5bd61cebd116cc812fd5488dd2e7467865471
dueityou <dueityou@gmail.com>
2025-04-12 13:20:22 +0900

    [change] from initialize() to connect() on youtube controller

--- app/javascript/controllers/youtube_controller.js
+++ app/javascript/controllers/youtube_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
                     "time-t",
   ]
 
-  initialize() {
+  connect() {
     var tag = document.createElement('script');
 
     tag.src = "https://www.youtube.com/iframe_api";
