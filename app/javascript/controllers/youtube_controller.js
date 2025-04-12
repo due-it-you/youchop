cc521d6939e942af6be0bb5fd88a60075e6fd27e
dueityou <dueityou@gmail.com>
2025-04-13 01:34:21 +0900

    [add] return target of key:M in the function targetTime

--- app/javascript/controllers/youtube_controller.js
+++ app/javascript/controllers/youtube_controller.js
@@ -116,6 +116,7 @@ export default class extends Controller {
     if(event.key == "j") return this.j_start_timeTarget
     if(event.key == "b") return this.b_start_timeTarget
     if(event.key == "n") return this.n_start_timeTarget
+    if(event.key == "m") return this.m_start_timeTarget
   }
 
   get getPlayer() {
