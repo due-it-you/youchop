8a19c6a5f3d92985099bf8c2781d765563823454
dueityou <dueityou@gmail.com>
2025-04-12 23:07:39 +0900

    [add] target the set time on y key

--- app/javascript/controllers/youtube_controller.js
+++ app/javascript/controllers/youtube_controller.js
@@ -7,6 +7,7 @@ export default class extends Controller {
                     "submit",
                     "frame",
                     "t_start_time",
+                    "y_start_time",
   ]
 
   initialize() {
