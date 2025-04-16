527a52127d2c3d433d98585aa63f6ef729ee7adc
dueityou <dueityou@gmail.com>
2025-04-16 15:48:13 +0900

    [add] static targets in step_sequencer.js

--- app/javascript/controllers/step_sequencer_controller.js
+++ app/javascript/controllers/step_sequencer_controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="step-sequencer"
 export default class extends Controller {
+
+  static targets = [
+                    
+  ]
+
   connect() {
   }
 }
