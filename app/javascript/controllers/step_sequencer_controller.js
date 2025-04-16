c0f1527ee672411a1da02cc2ff7952ecd98c5eee
dueityou <dueityou@gmail.com>
2025-04-16 15:54:12 +0900

    [add] targets bpm

--- app/javascript/controllers/step_sequencer_controller.js
+++ app/javascript/controllers/step_sequencer_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static targets = [
-                    
+                    "bpm",
   ]
 
   connect() {
