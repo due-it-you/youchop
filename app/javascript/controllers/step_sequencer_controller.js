c04a5979cf393fc2f3d22a4f0dd1902f11377b86
dueityou <dueityou@gmail.com>
2025-04-17 10:08:07 +0900

    [add] toggle active control when the step is clicked

--- app/javascript/controllers/step_sequencer_controller.js
+++ app/javascript/controllers/step_sequencer_controller.js
@@ -21,11 +21,26 @@ export default class extends Controller {
 
   stepActiveControl(event) {
     const stepClicked = event.target
-
-    if (stepClicked.getAttribute('active') == "false") {
-      stepClicked.classList.remove('bg-gray-400')
-      stepClicked.classList.add('bg-green-300')
-      stepClicked.setAttribute('active', 'true')
+    const isActive = stepClicked.dataset.active === "true"
+    
+    if (isActive == false) {
+      if (stepClicked.getAttribute('index') % 4 == 1) {
+        stepClicked.classList.remove('bg-gray-400')
+        stepClicked.classList.add('bg-green-300')
+      } else {
+        stepClicked.classList.add('bg-green-300')
+      }
     }
+
+    if (isActive == true) {
+      if (stepClicked.getAttribute('index') % 4 == 1) {
+        stepClicked.classList.remove('bg-green-300')
+        stepClicked.classList.add('bg-gray-400')
+      } else {
+        stepClicked.classList.remove('bg-green-300')
+      }
+    }
+
+    stepClicked.dataset.active = (!isActive).toString()
   }
 }
