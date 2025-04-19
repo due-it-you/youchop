7902e9bf896c83453b819373d410f56142bf67e4
dueityou <dueityou@gmail.com>
2025-04-20 00:08:28 +0900

    [change] variable name from firstDecimalStr to startTimeDecimalStr

--- app/javascript/controllers/youtube_controller.js
+++ app/javascript/controllers/youtube_controller.js
@@ -106,9 +106,9 @@ export default class extends Controller {
 
     const [m,s] = this.targetTime(event).start.value.split(":")
     const startTimeMinSec = [m,s]
-    const firstDecimalStr = this.targetTime(event).start_decimal.value
+    const startTimeDecimalStr = this.targetTime(event).start_decimal.value
+    const firstDecimalNum = Number('0.' + startTimeDecimalStr)
     const minSecArray = startTimeMinSec.map( str => parseInt(str, 10))
-    const firstDecimalNum = Number('0.' + firstDecimalStr)
     const totalSecondResult = minSecArray[0]*60 + minSecArray[1] + firstDecimalNum
 
     this.getPlayer.seekTo(totalSecondResult, true)
