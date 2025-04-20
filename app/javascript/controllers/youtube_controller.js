5ef4041d625d2e7a32ebde74e7fd3c627730d900
dueityou <dueityou@gmail.com>
2025-04-20 17:29:33 +0900

    [add] targetTime(event) return object includes each time input target of the pad on the key U

--- app/javascript/controllers/youtube_controller.js
+++ app/javascript/controllers/youtube_controller.js
@@ -168,7 +168,12 @@ export default class extends Controller {
       end:            this.g_end_timeTarget,
       end_decimal:    this.g_end_time_decimalTarget
     }
-    if(event.key == "h") return this.h_start_timeTarget
+    if(event.key == "h") return {
+      start: this.h_start_timeTarget,
+      start_decimal: this.h_start_time_decimalTarget,
+      end: this.h_end_timeTarget,
+      end_decimal: this.h_end_time_decimalTarget
+    }
     if(event.key == "j") return this.j_start_timeTarget
     if(event.key == "b") return this.b_start_timeTarget
     if(event.key == "n") return this.n_start_timeTarget
