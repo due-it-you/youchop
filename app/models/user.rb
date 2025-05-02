e19ad92d808fd2d324c255a07f2d1d7e814a8aec
dueityou <dueityou@gmail.com>
2025-05-02 22:32:34 +0900

    [add] dependent: :destroy into has_many :beats

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :beats
+  has_many :beats, dependent: :destroy
 end
