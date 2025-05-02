73a1ee57e80cda20af7a581f7ba65d668b7e2479
dueityou <dueityou@gmail.com>
2025-05-02 22:17:15 +0900

    [add] create_beats migration columns

--- db/migrate/20250502131159_create_beats.rb
+++ db/migrate/20250502131159_create_beats.rb
@@ -1,6 +1,9 @@
 class CreateBeats < ActiveRecord::Migration[7.2]
   def change
     create_table :beats do |t|
+      add_reference :beats, :user, null: false, foreign_key: true
+      t.string :title, null: false
+
       t.timestamps
     end
   end
