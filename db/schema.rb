4278a7c07909f5a8b920c214bc7c4e55cf45baad
dueityou <dueityou@gmail.com>
2025-05-03 19:23:33 +0900

    [add] migrate and create youtubes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_02_131936) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_03_102148) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,10 +30,19 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_02_131936) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "youtubes", force: :cascade do |t|
+    t.string "video_id"
+    t.bigint "beat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["beat_id"], name: "index_youtubes_on_beat_id"
+  end
+
   add_foreign_key "beats", "users"
+  add_foreign_key "youtubes", "beats"
 end
