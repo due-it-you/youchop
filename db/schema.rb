97fc6cf36f74e2a5174e4129aa9d9a25474f61b2
dueityou <dueityou@gmail.com>
2025-05-06 00:30:27 +0900

    [add] snares_active_index column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_05_145606) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_05_152936) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_05_145606) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "hihats_active_index", default: "", null: false
+    t.string "snares_active_index", default: "", null: false
     t.index ["beat_id"], name: "index_sequencers_on_beat_id"
   end
 
