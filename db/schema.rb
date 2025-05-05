5961a9a51acd8ec91c5d3a15e0490dddeb909388
dueityou <dueityou@gmail.com>
2025-05-06 00:33:23 +0900

    [add] kicks_active_index column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_05_152936) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_05_153044) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_05_152936) do
     t.datetime "updated_at", null: false
     t.string "hihats_active_index", default: "", null: false
     t.string "snares_active_index", default: "", null: false
+    t.string "kicks_active_index", default: "", null: false
     t.index ["beat_id"], name: "index_sequencers_on_beat_id"
   end
 
