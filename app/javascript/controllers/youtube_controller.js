d66612ad01a5e37a2711222b2c0d96dc23247911
dueityou <dueityou@gmail.com>
2025-05-06 03:00:08 +0900

    [add] kicks_volume: this.stepSequencerOutlet.kicks_volumeTarget.value

--- app/javascript/controllers/youtube_controller.js
+++ app/javascript/controllers/youtube_controller.js
@@ -186,7 +186,8 @@ export default class extends Controller {
       pad_active_index: padActiveIndexStr,
       youtube_volume: this.stepSequencerOutlet.pads_volumeTarget.value,
       hihats_volume: this.stepSequencerOutlet.hihats_volumeTarget.value,
-      snares_volume: this.stepSequencerOutlet.snares_volumeTarget.value
+      snares_volume: this.stepSequencerOutlet.snares_volumeTarget.value,
+      kicks_volume: this.stepSequencerOutlet.kicks_volumeTarget.value
     }
   }
 
