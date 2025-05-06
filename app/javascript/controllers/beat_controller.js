b7e93bbeb1fb2490f59291fa21a6c0447eb6f359
dueityou <dueityou@gmail.com>
2025-05-06 17:31:54 +0900

    [add] targets beat_title in the beat_controller

--- app/javascript/controllers/beat_controller.js
+++ app/javascript/controllers/beat_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="beat"
 export default class extends Controller {
+
+  static targets = [
+
+    "beat_title"
+
+  ]
+  
   connect() {
   }
 }
