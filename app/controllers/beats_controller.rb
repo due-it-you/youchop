c983e024f046525365fe0f55e5545f9be7a5d498
dueityou <dueityou@gmail.com>
2025-05-07 19:40:05 +0900

    [add] beat_params strong parameter

--- app/controllers/beats_controller.rb
+++ app/controllers/beats_controller.rb
@@ -1,2 +1,8 @@
 class BeatsController < ApplicationController
+
+  private
+
+  def beat_params
+    params.permit(:beat_title, :beats_data, :youtubes_data, :sequencers_data, :pad_timings_data)
+  end
 end
