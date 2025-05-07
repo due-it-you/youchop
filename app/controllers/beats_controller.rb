49874b294fb70815ddb82ca23ed6ac9a449f2d51
dueityou <dueityou@gmail.com>
2025-05-07 20:37:44 +0900

    [update] beat_params with adding require(:beat)

--- app/controllers/beats_controller.rb
+++ app/controllers/beats_controller.rb
@@ -3,6 +3,6 @@ class BeatsController < ApplicationController
   private
 
   def beat_params
-    params.permit(:beat_title, :beats_data, :youtubes_data, :sequencers_data, :pad_timings_data)
+    params.require(:beat).permit(:beat_title, :beats_data, :youtubes_data, :sequencers_data, :pad_timings_data)
   end
 end
