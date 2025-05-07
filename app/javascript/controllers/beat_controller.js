ebc26b05c1f2afb33354a6efc38c9be1d66d5757
dueityou <dueityou@gmail.com>
2025-05-07 16:58:37 +0900

    [add] this.beat_save_formTarget.requestSubmit()

--- app/javascript/controllers/beat_controller.js
+++ app/javascript/controllers/beat_controller.js
@@ -130,13 +130,14 @@ export default class extends Controller {
     return data_to_save
   }
 
-  createTheBeat (event) {
+  createTheBeat () {
     const data_to_save = this.setTheDataToSave()
 
     this.hidden_beats_data_fieldTarget.value = JSON.stringify(data_to_save.beats)
     this.hidden_youtubes_data_fieldTarget.value = JSON.stringify(data_to_save.youtubes)
     this.hidden_sequencers_data_fieldTarget.value = JSON.stringify(data_to_save.sequencers)
     this.hidden_pad_timings_data_fieldTarget.value = JSON.stringify(data_to_save.pad_timings)
-    // this.event.requestSubmit()
+
+    this.beat_save_formTarget.requestSubmit()
   }
 }
