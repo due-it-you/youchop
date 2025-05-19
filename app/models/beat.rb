c59570257fa0f08e1c4a344acde697f84ce406ca
dueityou <dueityou@gmail.com>
2025-05-19 14:46:14 +0900

    [change] maxlength from 27 to 35

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -1,5 +1,5 @@
 class Beat < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 27 }
+  validates :title, presence: true, length: { maximum: 35 }
 
   has_one :youtube, dependent: :destroy
   has_one :pad_timing, dependent: :destroy
